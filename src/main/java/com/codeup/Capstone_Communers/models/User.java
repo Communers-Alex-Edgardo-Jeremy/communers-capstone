4f4fd51e3678a62f914361629ad3c35b264fc5eb
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-04-10 12:18:16 -0500

    1218

---
+++ src/main/java/com/codeup/Capstone_Communers/models/User.java
@@ -0,0 +1,37 @@
+package com.codeup.Capstone_Communers.models;
+import jakarta.persistence.*;
+import lombok.*;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length=255, nullable = false)
+    private String first_name;
+
+    @Column(length=255, nullable = false)
+    private String last_name;
+
+    @Column(length=255, nullable = false)
+    private String username;
+
+    @Column(length=255, nullable = false)
+    private String email;
+
+    @Column(length=255, nullable = false)
+    private String password;
+
+    public User(User copy) {
+        id = copy.id;
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+}
