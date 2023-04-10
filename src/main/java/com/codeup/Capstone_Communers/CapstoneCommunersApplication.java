9146167571235bb0c8f83391a1a562a5325c7827
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-04-10 10:20:33 -0500

    1020-init

---
+++ src/main/java/com/codeup/Capstone_Communers/CapstoneCommunersApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.Capstone_Communers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneCommunersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneCommunersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/Capstone_Communers/CapstoneCommunersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.Capstone_Communers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneCommunersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
