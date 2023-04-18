a4b2ddf6167eb16cac3b7d1da32102419ce45063
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-04-18 12:35:21 -0500

    curl working

--- src/main/java/com/codeup/Capstone_Communers/repositories/UserRepository.java
+++ src/main/java/com/codeup/Capstone_Communers/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.codeup.Capstone_Communers.repositories;
 import com.codeup.Capstone_Communers.models.Post;
 import com.codeup.Capstone_Communers.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findById(long id);
     User findByUsername(String username);
 
+//    @Query("FROM User u JOIN follows f ON u.id = f.follower WHERE u.id = :id")
+//    List<User> findFollowing(long id);
 }
